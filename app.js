462154bd41dba89d1f22af27d3143dee4b99b96d
GhaithQ1 <Ghaithdrh@gmail.com>
2023-05-27 21:35:21 +0300

    ..

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const log = document.getElementById("logins")
 const namee = localStorage.getItem("name");
 const pas = localStorage.getItem("pasoord");
-const url = "./img/avatar.png";
 if(namee != "" && namee != null){
    log.innerHTML =`<h3> ${namee} </h3>`
-   log.innerHTML += `<img src ="${url}">`
-   log.innerHTML += `<i class="fa fa-caret-down" aria-hidden="true"></i>`
+   log.innerHTML += `<img src ="${"./img/avatar.png"}">`
+   log.innerHTML += `<i class="fa fa-caret-down" aria-hidden="true"></i>`;
+   log.innerHTML += `<buttun class="logout">LogOut</buttun>`;
    log.classList.add('name');
+   const logout =document.querySelector(".logout");
+   logout.addEventListener("click" ,()=>{
+   localStorage.clear();
+   window.location = "./index.html"
+});
 }
 
 const project = document.getElementById("pro");
-project.addEventListener("click" , ()=>{
+project.addEventListener("click" , (eo)=>{
    if(localStorage.length != 0){
       window.location = "./projects.html";
    }else{
